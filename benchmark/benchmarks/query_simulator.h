e77acaecc462181cba89c238b2de6d5820004ab8
fding <feng_ning_ding@yahoo.com>
2014-11-02 22:14:02 -0500

    added circular buffer code and TODO notes

--- benchmark/benchmarks/query_simulator.h
+++ benchmark/benchmarks/query_simulator.h
@@ -56,6 +56,46 @@ using std::deque;
 using std::min;
 using std::max;
 
+// templated class T, but really, only supports T=unsigned int
+// This class does not guarantee full consistency (some enqueues might get lost).
+template <class T, int BLOCKSIZE=16384-32>
+class circular_buffer {
+    private:
+        // An array of values. UINT_MAX indicates nothing is present,
+        T buffer[BLOCKSIZE];
+        int head;
+        int tail;
+    public:
+        circular_buffer():
+            head(0), tail(0)
+        {
+            memset(buffer, UINT_MAX, BLOCKSIZE*sizeof(T));
+        }
+
+        int enqueue(T x) {
+            buffer[tail] = x;
+            tail = (tail + 1) % BLOCKSIZE;
+            return 0;
+        }
+
+        // We don't really have epoch... epoch is maintained just for interface
+        void increment_epoch() {
+        }
+
+        unsigned int get_current_epoch() {
+            return 0;
+        }
+
+        int dequeue(T* ret, unsigned int* epoch) {
+            *ret = buffer[head];
+            *epoch = 0;
+            buffer[head] = UINT_MAX;
+            head = (head + 1) % BLOCKSIZE;
+            if (*ret == UINT_MAX) return 1;
+            return 0;
+        }
+};
+
 /* A queue implementation for producer-consumer problems.
  * All enqueues are assumed to happen in one thread,
  * and all dequeues in another.
@@ -95,23 +135,27 @@ class syncqueue {
             // TODO(fding): It would be helpful to add logging information here,
             // to see how fast dequeuing occurs relative to enqueuing.
             node* curr_node = head;
-	    int count = 0;
-	    unsigned int min_epoch = UINT_MAX;
-            unsigned int max_epoch = 0;
-	    while (curr_node != NULL) {
-		count++;
-		min_epoch = min(min_epoch, curr_node->epoch);
-		max_epoch = max(max_epoch, curr_node->epoch);
-                curr_node = curr_node->next;
-	    }
+            int count = 0;
+            unsigned int min_epoch = UINT_MAX;
+                unsigned int max_epoch = 0;
+            while (curr_node != NULL) {
+            count++;
+            min_epoch = min(min_epoch, curr_node->epoch);
+            max_epoch = max(max_epoch, curr_node->epoch);
+                    curr_node = curr_node->next;
+            }
 
-	    printf("count: %d\n", count);
-	    printf("epoch difference: %u\n", max_epoch - min_epoch);
-            if (tmp_head_section) {
+            printf("count: %d\n", count);
+            printf("epoch difference: %u\n", max_epoch - min_epoch);
+                if (tmp_head_section) {
                 free(tmp_head_section);
             }
         }
 
+        // TODO(fding): this function keeps checking head,
+        // which is always changed by dequeue.
+        // I don't think this is good for the cache,
+        // as dequeue and enqueue probably runs on different cores.
         int enqueue(T x) {
             int i;
             if (tail == BLOCKSIZE - 1) {
@@ -281,6 +325,13 @@ public:
 		    edge_t last = first + (*vtable)[add].level_length;
 		    if (last - first > 0) etable->advise(first, last);
 
+            // TODO(fding): We should experiment moving this sequence of madvises
+            // to a different #pragma omp section.
+            // One thought: have two queues, one for each madvise thread.
+            // In the query thread, enqueue node to both threads.
+            // In one madvise thread, advise the node (so that its corresponding queue is emptied faster).
+            // In other madvise thread, advise the friends of the node,
+            // but only if the thread is not too far behind.
 		    FOREACH_OUTEDGE_ITER(v_idx, G, iteradd) {
                             node_t next_node = iteradd.last_node;
                             edge_t first = (*vtable)[next_node].adj_list_start;
