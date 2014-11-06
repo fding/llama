d1b7ed9601a3ab7a86411250cc1e49203ad3d08a
Albert Wu <awu@college.harvard.edu>
2014-11-05 23:27:47 -0500

    output stuff

--- benchmark-runner.sh
+++ benchmark-runner.sh
@@ -3,7 +3,6 @@ EPOCH_VALUES=(0 1 2 3 4)
 NVERT_VALUES=(1000 5000 10000 20000 40000)
 CACHE_VALUES=(50 100 200 500 1000 2000)
 
-
 function run() {
 # $1 is alpha, $2 is epoch, $3 is num_vertices, $4 is cache size
     echo "==========START EXPERIMENT==========" >> output.log
@@ -14,34 +13,36 @@ function run() {
     cp -f benchmark/benchmarks/query_simulator.h asdftmp.h
     for param in PARAM_ALPHA PARAM_EPOCH_THRESHOLD PARAM_NUM_VERTICES PARAM_CACHE_SIZE
     do
-        sed -i "s/^#define $param.*$//g" benchmark/benchmarks/query_simulator.h
+        sed -i ".bak" "s/^#define $param .*$//g" benchmark/benchmarks/query_simulator.h
     done
 
-    sed -i "s/PARAM_ALPHA/$1/g" benchmark/benchmarks/query_simulator.h
-    sed -i "s/PARAM_EPOCH_THRESHOLD/$2/g" benchmark/benchmarks/query_simulator.h
-    sed -i "s/PARAM_NUM_VERTICES/$3/g" benchmark/benchmarks/query_simulator.h
-    sed -i "s/PARAM_CACHE_SIZE/$4/g" benchmark/benchmarks/query_simulator.h
+    sed -i ".bak" "s/PARAM_ALPHA/$1/g" benchmark/benchmarks/query_simulator.h
+    sed -i ".bak" "s/PARAM_EPOCH_THRESHOLD/$2/g" benchmark/benchmarks/query_simulator.h
+    sed -i ".bak" "s/PARAM_NUM_VERTICES/$3/g" benchmark/benchmarks/query_simulator.h
+    sed -i ".bak" "s/PARAM_CACHE_SIZE/$4/g" benchmark/benchmarks/query_simulator.h
 
     make benchmark-persistent benchmark-persistent-madvise
 
-    echo "PARAM_ALPHA=$1,PARAM_EPOCH_THRESHOLD=$2,PARAM_NUM_VERTICES=$3,PARAM_CACHE_SIZE=$4" >> output.og
+    echo "PARAM_ALPHA=$1,PARAM_EPOCH_THRESHOLD=$2,PARAM_NUM_VERTICES=$3,PARAM_CACHE_SIZE=$4" >> output.log
 
     cd bin
     echo "NO_MADVISE" >> ../output.log
-    for i in {1..10}
+    for m in {1..10}
     do
-        echo "TRIAL $i" >> ../output.log
+        echo "TRIAL $m" >> ../output.log
         echo "==========LLAMA OUTPUT==========" >> ../output.log
-        benchmark-persistent --run query_simulator -d db/ >> ../output.log
+	sudo purge
+        ./benchmark-persistent --run query_simulator -d db/ >> ../output.log
         echo "==========END LLAMA OUTPUT==========" >> ../output.log
     done
 
-    echo "WITH_MADVISE" >> output.log
-    for i in {1..10}
+    echo "WITH_MADVISE" >> ../output.log
+    for n in {1..10}
     do
-        echo "TRIAL $i" >> ../output.log
+        echo "TRIAL $n" >> ../output.log
         echo "==========LLAMA OUTPUT==========" >> ../output.log
-        benchmark-persistent-madvise --run query_simulator -d db/ >> ../output.log
+	sudo purge
+        ./benchmark-persistent-madvise --run query_simulator -d db/ >> ../output.log
         echo "==========END LLAMA OUTPUT==========" >> ../output.log
     done
     cd ..
