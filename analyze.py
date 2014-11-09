a71d0150ffbb71ad01ab47257a77bb13662c0726
fding <feng_ning_ding@yahoo.com>
2014-11-08 22:13:08 -0500

    code cleanup; preparation for albert's new log format

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+import re
 f = open('output.log', 'r')
 
 class Experiment(object):
@@ -12,40 +13,42 @@ class Experiment(object):
 lines = f.read().split('\n')
 i = 0
 
+def wait_for_line(s, i):
+    p = re.compile(s)
+    while not p.match(lines[i].strip()):
+        i = i + 1
+    return i
+
 experiments = []
+with_madvise = False
+
 while i < len(lines):
-    line = lines[i]
-    line = line.strip()
-    if line != '==========START EXPERIMENT==========':
-        i += 1
-        continue
+    i = wait_for_line('==========START EXPERIMENT==========', i)
     a = Experiment(lines[i+1], lines[i+2])
+    i = i + 3
 
     for j in range(5):
-        while lines[i] != '==========LLAMA OUTPUT==========':
-            i += 1
+        i = wait_for_line('(NO_MADVISE)|(WITH_MADVISE)', i)
+        if lines[i] == 'WITH_ADVISE':
+            with_madvise = True
+        else:
+            with_madvise = False
+
+        i = wait_for_line('TRIAL \d+', i)
+        i = wait_for_line('==========LLAMA OUTPUT==========', i)
+        j = wait_for_line('==========END LLAMA OUTPUT==========', i)
         output = {}
-        i += 1
-        while lines[i] != '==========END LLAMA OUTPUT==========':
-            parts = lines[i].split(':')
+        for line in lines[i+1: j]:
+            parts = line.split(':')
             if len(parts) == 2:
                 output[parts[0].strip()] = parts[1].strip()
-            i += 1
-        i += 1
-        a.no_madvise_outputs.append(output)
-            
-    for j in range(5):
-        while lines[i] != '==========LLAMA OUTPUT==========':
-            i += 1
-        output = {}
-        i += 1
-        while lines[i] != '==========END LLAMA OUTPUT==========':
-            parts = lines[i].split(':')
-            if len(parts) == 2:
-                output[parts[0].strip()] = parts[1].strip()
-            i += 1
-        i += 1
-        a.with_madvise_outputs.append(output)
+        i = j + 1
+
+        if with_madvise:
+            a.with_madvise_outputs.append(output)
+        else:
+            a.no_madvise_outputs.append(output)
+
     experiments.append(a)
 
 def floatify(s):
